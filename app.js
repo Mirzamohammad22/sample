9081b375ff713a1293092d073143916fcab44578
Mirzamohammad22 <Mirza.Mohammad19@hotmail.com>
2020-06-15 15:12:36 +0400

    testing lighthouse action

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+let {lighthouseCheck} = require('@foo-software/lighthouse-check')
+const fs = require('fs')
+const ev = JSON.parse(
+  fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')
+)
+const prNum = ev.pull_request.number
+console.log('prNum',prNum)
+async function mylighthouse(){
+
+    const response = await lighthouseCheck({
+        urls: [
+          'https://www.dubizzle.com'
+        ]
+      });
+}
+mylighthouse()
