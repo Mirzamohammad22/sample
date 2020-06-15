cc67ae8ea5948854ead8fd14788620a7d5c3d3c5
Mirzamohammad22 <Mirza.Mohammad19@hotmail.com>
2020-06-15 16:02:48 +0400

    testing lighthouse

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 let {lighthouseCheck} = require('@foo-software/lighthouse-check')
-
-console.log('prNum',prNum)
 async function mylighthouse(){
-
     const response = await lighthouseCheck({
         urls: [
           'https://www.dubizzle.com'
