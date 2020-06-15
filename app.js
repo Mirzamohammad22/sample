e6a6e218eab4f2a6ea30c0f7309cded3c27fb677
Mirzamohammad22 <Mirza.Mohammad19@hotmail.com>
2020-06-15 15:25:04 +0400

    testing pull request number

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let {lighthouseCheck} = require('@foo-software/lighthouse-check')
-const fs = require('fs')
-const ev = JSON.parse(
-  fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')
-)
+// const fs = require('fs')
+// const ev = JSON.parse(
+//   fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')
+// )
 const prNum = ev.pull_request.number
 console.log('prNum',prNum)
 async function mylighthouse(){
