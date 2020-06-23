7271b902456f4f916dfa44289dd66cf2ce9e948f
Mirzamohammad22 <Mirza.Mohammad19@hotmail.com>
2020-06-23 18:09:16 +0400

    Testing lighthouse

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-let {lighthouseCheck} = require('@foo-software/lighthouse-check')
-async function mylighthouse(){
-    const response = await lighthouseCheck({
-        urls: [
-          'https://www.dubizzle.com'
-        ]
-      });
+let { lighthouseCheck } = require('@foo-software/lighthouse-check')
+
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+const GITHUB_REPO = process.env.GITHUB_REPO
+const GITHUB_REF = process.env.GITHUB_REF 
+
+const parsePullRequestId = githubRef => {
+  const result = /refs\/pull\/(\d+)\/merge/g.exec(githubRef);
+  if (!result) throw new Error("Reference not found.");
+  const [, pullRequestId]= result;
+  return pullRequestId;
+};
+async function mylighthouse() {
+  const PR = parsePullRequestId(GITHUB_REF)
+  const URL = `https://api.github.com/repos/${GITHUB_REPO}/pulls/${PR}/reviews`
+  lighthouseCheck({
+    emulatedFormFactor: 'both',
+    prCommentSaveOld: false,
+    prCommentAccessToken: GITHUB_TOKEN,
+    prCommentUrl:URL ,
+    urls: [
+      'https://www.dubizzle.com'
+    ]
+  });
 }
 mylighthouse()
